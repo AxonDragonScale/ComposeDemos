2caedcbb57329e6baf9dd2b580bd5b777ccebc25
AxonDragonScale <r.r.n.harkhani@gmail.com>
2024-04-25 20:09:45 +0530

    Add RingedCirclesLoader

--- app/src/main/java/com/axondragonscale/compose/demo/loader/Loaders.kt
+++ app/src/main/java/com/axondragonscale/compose/demo/loader/Loaders.kt
@@ -1,7 +1,6 @@
 package com.axondragonscale.compose.demo.loader
 
 import android.content.res.Configuration
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -14,12 +13,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.axondragonscale.compose.demo.ui.theme.ComposeDemosTheme
@@ -49,6 +46,10 @@ fun Loaders(modifier: Modifier = Modifier) {
         Spacer(modifier = Modifier.height(24.dp))
         LoaderContainer(Modifier.height(48.dp)) {
             NewtonCradleLoader()
+        }
+        Spacer(modifier = Modifier.height(24.dp))
+        LoaderContainer(Modifier.height(100.dp)) {
+            RingedCirclesLoader()
         }
     }
 }

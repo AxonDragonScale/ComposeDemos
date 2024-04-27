0e5c3e8bb75c4aba0062b0352dd19b40ac813be5
AxonDragonScale <r.r.n.harkhani@gmail.com>
2024-04-27 19:42:35 +0530

    Add transition animations for navigation

--- app/src/main/java/com/axondragonscale/compose/demo/ui/DemoApp.kt
+++ app/src/main/java/com/axondragonscale/compose/demo/ui/DemoApp.kt
@@ -1,5 +1,6 @@
 package com.axondragonscale.compose.demo.ui
 
+import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -24,7 +25,19 @@ fun DemoApp() {
     val navController = rememberNavController()
     NavHost(
         navController = navController,
-        startDestination = Route.Home.route
+        startDestination = Route.Home.route,
+        enterTransition = {
+            slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Left)
+        },
+        exitTransition = {
+            slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.Left)
+        },
+        popEnterTransition = {
+            slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Right)
+        },
+        popExitTransition = {
+            slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.Right)
+        }
     ) {
         composable(route = Route.Home.route) {
             Home(navController = navController)

f141445d6c1c29a1d1c8b1fe0f777364526640a5
AxonDragonScale <r.r.n.harkhani@gmail.com>
2025-07-09 00:48:38 +0530

    Migrate disabled Shaders.kt to KMP

---
+++ composeApp/src/commonMain/kotlin/com/axondragonscale/compose/demo/shaders/PositionalShader.kt
@@ -0,0 +1,73 @@
+package com.axondragonscale.compose.demo.shaders
+
+//import android.content.res.Configuration
+//import android.graphics.RenderEffect
+//import android.graphics.RuntimeShader
+//import android.os.Build
+//import androidx.annotation.RequiresApi
+//import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.material3.Surface
+//import androidx.compose.runtime.Composable
+//import androidx.compose.runtime.remember
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.graphics.asComposeRenderEffect
+//import androidx.compose.ui.graphics.graphicsLayer
+//import androidx.compose.ui.layout.onSizeChanged
+//import androidx.compose.ui.tooling.preview.Preview
+//import com.axondragonscale.compose.demo.ui.theme.ComposeDemosTheme
+//
+///**
+// * Created by Ronak Harkhani on 10/05/24
+// */
+//
+//@RequiresApi(Build.VERSION_CODES.TIRAMISU)
+//val PositionalShaderPage = ShaderPage(
+//    title = "Positional Shader",
+//    desc = "Calculates color of each pixel based on its position.",
+//    content = { PositionalShader() }
+//)
+//
+//private val SHADER = """
+//    uniform float2 size;
+//
+//    half4 main(float2 pos) {
+//        // Define how to calculate r, g, b, a values for each pixel and return as half4
+//        // Alt notation: half4((pos / size).xy, 0.75, 1)
+//        return half4(pos.x / size.x, pos.y / size.y, 0.75, 1);
+//    }
+//
+//""".trimIndent()
+//
+//@RequiresApi(Build.VERSION_CODES.TIRAMISU)
+//@Composable
+//fun PositionalShader(modifier: Modifier = Modifier) {
+//    val shader = remember { RuntimeShader(SHADER) }
+//    Surface(
+//        modifier = modifier
+//            .fillMaxSize()
+//            .onSizeChanged {
+//                shader.setFloatUniform(
+//                    "size",
+//                    it.width.toFloat(),
+//                    it.height.toFloat()
+//                )
+//            }
+//            .graphicsLayer {
+//                renderEffect = RenderEffect
+//                    .createShaderEffect(shader)
+//                    .asComposeRenderEffect()
+//            }
+//    ) {}
+//}
+//
+//@RequiresApi(Build.VERSION_CODES.TIRAMISU)
+//@Preview(name = "Light", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_NO)
+//@Preview(name = "Dark", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+//@Composable
+//private fun Preview() {
+//    ComposeDemosTheme {
+//        Surface {
+//            PositionalShader()
+//        }
+//    }
+//}
---
+++ composeApp/src/commonMain/kotlin/com/axondragonscale/compose/demo/shaders/Shaders.kt
@@ -0,0 +1,132 @@
+package com.axondragonscale.compose.demo.shaders
+
+//import android.content.res.Configuration
+//import android.os.Build
+//import androidx.annotation.RequiresApi
+//import androidx.compose.foundation.ExperimentalFoundationApi
+//import androidx.compose.foundation.layout.Arrangement
+//import androidx.compose.foundation.layout.Box
+//import androidx.compose.foundation.layout.Column
+//import androidx.compose.foundation.layout.Row
+//import androidx.compose.foundation.layout.Spacer
+//import androidx.compose.foundation.layout.fillMaxHeight
+//import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.foundation.layout.fillMaxWidth
+//import androidx.compose.foundation.layout.padding
+//import androidx.compose.foundation.pager.HorizontalPager
+//import androidx.compose.foundation.pager.VerticalPager
+//import androidx.compose.foundation.pager.rememberPagerState
+//import androidx.compose.material.icons.Icons
+//import androidx.compose.material.icons.automirrored.filled.ArrowBack
+//import androidx.compose.material.icons.automirrored.filled.ArrowBackIos
+//import androidx.compose.material.icons.automirrored.filled.ArrowForward
+//import androidx.compose.material.icons.automirrored.filled.ArrowForwardIos
+//import androidx.compose.material.icons.automirrored.filled.ArrowLeft
+//import androidx.compose.material.icons.automirrored.filled.ArrowRight
+//import androidx.compose.material.icons.filled.ArrowBack
+//import androidx.compose.material3.Icon
+//import androidx.compose.material3.IconButton
+//import androidx.compose.material3.MaterialTheme
+//import androidx.compose.material3.Surface
+//import androidx.compose.material3.Text
+//import androidx.compose.runtime.Composable
+//import androidx.compose.runtime.LaunchedEffect
+//import androidx.compose.runtime.rememberCoroutineScope
+//import androidx.compose.runtime.snapshotFlow
+//import androidx.compose.ui.Alignment
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.text.font.FontWeight
+//import androidx.compose.ui.tooling.preview.Preview
+//import androidx.compose.ui.unit.dp
+//import com.axondragonscale.compose.demo.ui.theme.ComposeDemosTheme
+//import kotlinx.coroutines.launch
+//
+///**
+// * Created by Ronak Harkhani on 10/05/24
+// */
+//
+//data class ShaderPage(
+//    val title: String,
+//    val desc: String,
+//    val content: @Composable () -> Unit,
+//)
+//
+//@RequiresApi(Build.VERSION_CODES.TIRAMISU)
+//private val pages = listOf(
+//    PositionalShaderPage,
+//    TextShaderPage,
+//    ImageShaderPage,
+//    TimeShaderPage,
+//)
+//
+//@OptIn(ExperimentalFoundationApi::class)
+//@Composable
+//fun Shaders(modifier: Modifier = Modifier) = Column(modifier = modifier.fillMaxSize()) {
+//    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) return@Column
+//
+//    val scope = rememberCoroutineScope()
+//    val pagerState = rememberPagerState { pages.size }
+//    Box(
+//        modifier = Modifier
+//            .weight(1f)
+//            .padding(vertical = 64.dp, horizontal = 32.dp)
+//    ) {
+//        pages[pagerState.currentPage].content()
+//        HorizontalPager(modifier = Modifier.fillMaxSize(), state = pagerState) {}
+//    }
+//
+//    Row(
+//        modifier = Modifier.padding(bottom = 32.dp),
+//        verticalAlignment = Alignment.CenterVertically,
+//    ) {
+//        IconButton(
+//            onClick = {
+//                scope.launch {
+//                    pagerState.scrollToPage(pagerState.currentPage - 1)
+//                }
+//            },
+//            enabled = pagerState.currentPage != 0
+//        ) {
+//            Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBackIos, contentDescription = "")
+//        }
+//
+//        Column(
+//            modifier = Modifier.weight(1f),
+//            verticalArrangement = Arrangement.Center,
+//        ) {
+//            Text(
+//                text = pages[pagerState.currentPage].title,
+//                style = MaterialTheme.typography.titleLarge,
+//                fontWeight = FontWeight.Thin,
+//            )
+//
+//            Text(
+//                text = pages[pagerState.currentPage].desc,
+//                style = MaterialTheme.typography.labelLarge,
+//                fontWeight = FontWeight.Bold,
+//            )
+//        }
+//
+//        IconButton(
+//            onClick = {
+//                scope.launch {
+//                    pagerState.scrollToPage(pagerState.currentPage + 1)
+//                }
+//            },
+//            enabled = pagerState.currentPage != pages.lastIndex,
+//        ) {
+//            Icon(imageVector = Icons.AutoMirrored.Filled.ArrowForwardIos, contentDescription = "")
+//        }
+//    }
+//}
+//
+//@Preview(name = "Light", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_NO)
+//@Preview(name = "Dark", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+//@Composable
+//private fun Preview() {
+//    ComposeDemosTheme {
+//        Surface {
+//            Shaders()
+//        }
+//    }
+//}
